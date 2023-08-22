0d733fed4c6b6556333e4b20a1011b1e515bed20
2a3b4c <839435510@qq.com>
2023-08-22 07:36:28 +0000

    parametering

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# 1111
+# M-Control: Improving Text-Image Consistency via Mask-Guided ControlNet
+
+![teaser](./assets/teaser.png)
+
+## Usage
+
+**To plug-and-play diffusion features, please follow these steps:**
+
+1. [Setup](#setup)
+2. [Mask-Guided ControlNet](#inference)
+
+## Setup
+
+Our codebase is built on [huggingface/diffusers](https://github.com/huggingface/diffusers)
+and has shared environmental dependencies.
+
+### Creating a Conda Environment
+
+```
+conda env create -f environment.yaml
+conda activate M-Control
+```
+
+## Mask-Guided ControlNet
+
+If you already have a condition input and corresponding mask, first set the hyper-parameters for the generation in a yaml config file. An example of extraction configs can be found in `configs/test.yaml`. Once the arguments are set, run:
+
+```
+python inference.py --config <extraction_config_path>
+```
--- inference.py
+++ inference.py
@@ -13,28 +13,74 @@ from math import sqrt
 import warnings
 warnings.filterwarnings("ignore")
 import json
+import argparse
+from omegaconf import OmegaConf
 
-path = "SG161222/Realistic_Vision_V2.0"
-controlnet = ControlNetModel.from_pretrained("lllyasviel/control_v11f1p_sd15_depth", torch_dtype=torch.float).to("cuda:0")
-pipe_canny = StableDiffusionControlNetPipelineMControl.from_pretrained(path, controlnet=controlnet, local_files_only=True, torch_dtype=torch.float).to("cuda:0")
-euler_scheduler = DPMSolverMultistepScheduler.from_config(path, subfolder="scheduler")
-# euler_scheduler = DDIMScheduler.from_config(path, subfolder="scheduler")
-pipe_canny.scheduler = euler_scheduler
-pipe_canny.safety_checker=None
+def main():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument(
+        "--config",
+        type=str,
+        default="../configs/test.yaml",
+        help="path to the feature extraction config file"
+    )
+    parser.add_argument(
+        "--H",
+        type=int,
+        default=512,
+        help="height of generating images",
+    )
+    parser.add_argument(
+        "--W",
+        type=int,
+        default=512,
+        help="width of generating images",
+    )
+    parser.add_argument(
+        "--cnet",
+        type=str,
+        default="lllyasviel/control_v11f1p_sd15_depth",
+        help="path to ControlNet model",
+    )
+    parser.add_argument(
+        "--sd",
+        type=str,
+        default="SG161222/Realistic_Vision_V2.0",
+        help="path to StableDiffusion model",
+    )
 
-root_path = "./examples/table_depth/"
-dir = os.listdir(root_path) 
+    model_config = parser.parse_args()
+    setup_config = OmegaConf.load("./configs/test.yaml")
+    print(setup_config)
+    # model load
+    controlnet = ControlNetModel.from_pretrained(model_config.cnet, torch_dtype=torch.float).to("cuda:0")
+    pipe_canny = StableDiffusionControlNetPipelineMControl.from_pretrained(model_config.sd, controlnet=controlnet, local_files_only=True, torch_dtype=torch.float).to("cuda:0")
+    euler_scheduler = DPMSolverMultistepScheduler.from_config(model_config.sd, subfolder="scheduler")
+    height = model_config.H
+    width = model_config.W
+    pipe_canny.scheduler = euler_scheduler
+    pipe_canny.safety_checker=None
+    
 
-height = 512
-width = 512
-
-for path in dir:
-    depth_image = Image.open(root_path + path)
-    mask_image = Image.open("./mask_examples/mask_table22.png") 
-
+    depth_image = Image.open(setup_config.cindition_path)
+    mask_image = Image.open(setup_config.mask_path) 
+    prompt = setup_config.prompt
+    inject_step = setup_config.guidance_threshold
+    num_inference_steps = setup_config.num_ddim_sampling_steps
+    guidance_scale = setup_config.scale
+    alpha = setup_config.alpha
+    token_index = setup_config.token_index
+    inject_padding_num = setup_config.inject_padding_num
+    
     generator = torch.Generator(device="cpu").manual_seed(123)
-    output_our, _ = pipe_canny(prompt='an apple on the table', c_encoder=3, u_decoder=4, 
-                    image=depth_image, inject_step=20, user_local_edit=False, noise_input1=None, 
-                    num_inference_steps=30, guidance_scale=9, num_images_per_prompt=1, controlnet_conditioning_scale=float(1),
-                    generator=generator, mask=mask_image, height=height, width=width, alpha=0.5, inject_padding_num=5, token_index=[0, 2])
+    output_our, _ = pipe_canny(prompt=prompt, c_encoder=3, u_decoder=4, 
+                    image=depth_image, inject_step=inject_step, user_local_edit=False, noise_input1=None, 
+                    num_inference_steps=num_inference_steps, guidance_scale=guidance_scale, 
+                    num_images_per_prompt=1, controlnet_conditioning_scale=float(1),
+                    generator=generator, mask=mask_image, height=height, width=width, alpha=alpha, inject_padding_num=inject_padding_num, token_index=token_index)
     output_our.images[0].save("./outputs/our.png")
+
+
+if __name__ == "__main__":
+    main()
